1e2117bd229d684090084eed186ff7ee6f876c0a
Emmanuel Nhan <nhanmanu@gmail.com>
2012-05-11 23:11:56 +0200

    added logging and a basic TimelordUser class

---
+++ src/main/java/eu/enhan/timelord/domain/core/TimelordUser.java
@@ -0,0 +1,91 @@
+/*
+ *   This file is part of Timelord.
+ *
+ *   Timelord is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Timelord is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the Affero GNU General Public License
+ *   along with Timemord.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.enhan.timelord.domain.core;
+
+/**
+ * Represents a user for the Timelord application.
+ * 
+ * @author Emmanuel Nhan
+ *
+ */
+public class TimelordUser {
+	
+	private String login;
+	private String password;
+	private String email;
+	
+	
+	public String getLogin() {
+		return login;
+	}
+
+    public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result
+				+ ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TimelordUser other = (TimelordUser) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+	
+	
+
+}
