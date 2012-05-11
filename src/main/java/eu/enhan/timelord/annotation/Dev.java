cc0b52a5244b29925b2fd86d19c8d8694cc0669f
Emmanuel Nhan <nhanmanu@gmail.com>
2012-05-11 23:58:33 +0200

    added annotations as a base for spring profiles

---
+++ src/main/java/eu/enhan/timelord/annotation/Dev.java
@@ -0,0 +1,40 @@
+/*
+ *   This file is part of Timelord.
+ *
+ *   Timelord is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Timelord is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the Affero GNU General Public License
+ *   along with Timemord.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.enhan.timelord.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
+/**
+ * Shortcut annotation to use on {@link Configuration} classes.
+ * {@link Dev} indicates that the class must be activated when the dev profile is activated
+ * 
+ * @author Emmanuel Nhan
+ *
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Profile("dev")
+public @interface Dev {
+
+}
